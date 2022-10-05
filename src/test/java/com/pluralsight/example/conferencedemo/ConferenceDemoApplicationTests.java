91e5bd1155ba891b1dd3e69975360d15145bc114
Bear-Dev <laidinhthuan123@gmail.com>
2022-10-05 11:54:14 +0700

    Initial commit

---
+++ src/test/java/com/pluralsight/example/conferencedemo/ConferenceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.example.conferencedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
